165f422d26189ece3edcbcb3d41c1bbacdf7bd28
andrewkimjoseph <andrewkimjoseph@gmail.com>
2024-11-11 19:27:33 +0300

    Update clients

--- bekwest/packages/react-app/utils/clients/private.tsx
+++ bekwest/packages/react-app/utils/clients/private.tsx
@@ -1,10 +1,21 @@
 import { createWalletClient, custom } from "viem";
 import { celoAlfajores } from "viem/chains";
 
-export const privateClient = createWalletClient({
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+let privateClient;
+
+if (typeof window !== "undefined" && window.ethereum) {
+  privateClient = createWalletClient({
     chain: celoAlfajores,
     transport: custom(window.ethereum),
   });
+} else {
+  console.error("Ethereum provider is not available.");
+}
 
-
-  
+export { privateClient };
--- bekwest/packages/react-app/utils/clients/public.tsx
+++ bekwest/packages/react-app/utils/clients/public.tsx
@@ -1,7 +1,21 @@
 import { createPublicClient, custom } from "viem";
 import { celoAlfajores } from "viem/chains";
 
-export const publicClient = createPublicClient({
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+let publicClient;
+
+if (typeof window !== "undefined" && window.ethereum) {
+  publicClient = createPublicClient({
     chain: celoAlfajores,
     transport: custom(window.ethereum),
   });
+} else {
+  console.error("Ethereum provider is not available.");
+}
+
+export { publicClient };
